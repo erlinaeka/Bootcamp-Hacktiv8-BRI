5ae00d2f5b259b8001a90cc3a986798a99e5e9bc
erlinaefit@gmail.com <ekaerlina798@gmail.com>
2024-01-31 08:41:33 +0700

    add latihan 1 day 3

--- Day 3/Source Code/Latihan 1/latihan1.js
+++ Day 3/Source Code/Latihan 1/latihan1.js
@@ -1,1 +1,19 @@
-console.log("Erlina Eka Fitriani")
+// 1. Palindrom
+
+function cekPalindrom(kata){
+    let frase = kata.replace(/[^A-Z0-9]+/ig, '').toLowerCase();
+    // menghilangkan karakter spesial dan mengubah menjadi huruf kecil semua untuk mengabaikan besar kecilnya huruf
+    // console.log(frase)
+    let arrayFrase = frase.split('');
+    arrayFrase = arrayFrase.reverse().join('');
+    return arrayFrase == frase;
+}
+
+console.log(cekPalindrom("Katak"));
+console.log(cekPalindrom("Kasur Ini Rusak"));
+console.log(cekPalindrom("Level"));
+console.log(cekPalindrom("Malam"));
+console.log(cekPalindrom("A man, a plan, a canal, Panama!"));
+console.log(cekPalindrom("Coding"));
+
+
